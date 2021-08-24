e6e2e6cb1ef292974c8faf7cf43cb2afe7394992
adbenitez <asieldbenitez@gmail.com>
2021-08-24 15:50:20 -0400

    update filter description

--- simplebot_downloader/__init__.py
+++ simplebot_downloader/__init__.py
@@ -26,7 +26,7 @@ def deltabot_init(bot: DeltaBot) -> None:
 
 @simplebot.filter
 def download_link(bot: DeltaBot, message: Message) -> None:
-    """Send me in private any direct download link to download file.
+    """Send me in private any direct download link and I will send you the file.
 
     Example:
     https://example.com/path/to/file.zip
