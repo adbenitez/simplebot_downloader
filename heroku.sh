a418335ff26dab4a8f8dc1233d773c039cfbd2ad
adbenitez <asieldbenitez@gmail.com>
2022-04-26 19:16:12 -0400

    don't disable encryption, use encryption_error plugin to recover from key changes

--- heroku.sh
+++ heroku.sh
@@ -3,12 +3,15 @@
 # configure the bot
 python3 -m simplebot init "$ADDR" "$PASSWORD"
 python3 -m simplebot -a "$ADDR" set_name "File Downloader"
-python3 -m simplebot -a "$ADDR" set_config e2ee_enabled 0  # disable encryption to avoid issues with key lost
 
 # add the youtube plugin
 python3 -m pip install youtube-dl # required by youtube.py plugin
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/youtube.py'); open('youtube.py', 'wb').write(r.content)"
 python3 -m simplebot -a "$ADDR" plugin --add ./youtube.py
+
+# add the encryption_error plugin to leverage key changes
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/encryption_error.py'); open('encryption_error.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./encryption_error.py
 
 # add admin plugin
 if [ -n "$ADMIN" ]; then
